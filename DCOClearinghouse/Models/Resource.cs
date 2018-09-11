d712722feb4db13fec484b630466a3128b1b8b55
Aaron Tong <atong@osisoft.com>
2018-09-11 11:45:00 -0400

    Add models

---
+++ DCOClearinghouse/Models/Resource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DCOClearinghouse.Models
+{
+    public enum ResourceStatus
+    {
+        New = 0,
+        Approved,
+        Removed
+    }
+    public class Resource
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public int CategoryID { get; set; }
+        public int SubcategoryID { get; set; }
+        public int BadlinkVotes { get; set; }
+        public DateTime CreateDate { get; set; }
+        public ResourceStatus Status { get; set; }
+        public ResourceCategory Category { get; set; }
+        public ResourceSubcategory Subcategory { get; set; }
+    }
+}
---
+++ DCOClearinghouse/Models/ResourceCategory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DCOClearinghouse.Models
+{
+    public class ResourceCategory
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+
+        public ICollection<ResourceSubcategory> Subcategories { get; set; }
+    }
+}
